c40ee28419bdc932f67d1fde0afb0c28a1ac35b5
dgdeleonardis <diegowazzaap@hotmail.it>
2015-04-05 16:32:53 +0200

    Commit

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main(int argc, char** argv){
     for(i = 0; i < N; i++) {
         indiceMinimo = i;
         for(j = i + 1; j < N; j++) { 
-            if (v[indiceMinimo] > v[j]) {
+            if (v[indiceMinimo] < v[j]) {
                 indiceMinimo = j;
             }
         }
